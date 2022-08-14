cd626755ff1a84c54660939a7e9715ec998e1c3e
Maaz Bin Salman <63048405+maazbinsalman@users.noreply.github.com>
2022-08-14 08:17:06 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SimpleMinefield
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblBomb = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(296, 339);
+            this.flowLayoutPanel1.TabIndex = 0;
+            
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(347, 79);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(98, 39);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Begin";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(343, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score";
+            
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(343, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bomb!!";
+            
+            // 
+            // lblBomb
+            // 
+            this.lblBomb.AutoSize = true;
+            this.lblBomb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblBomb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBomb.Location = new System.Drawing.Point(425, 207);
+            this.lblBomb.Name = "lblBomb";
+            this.lblBomb.Size = new System.Drawing.Size(20, 24);
+            this.lblBomb.TabIndex = 2;
+            this.lblBomb.Text = "0";
+            
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.LightGreen;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblScore.Location = new System.Drawing.Point(425, 180);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(20, 24);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "0";
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 364);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblBomb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minefield";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblBomb;
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleMinefield
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int mineScore;
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            //int mineScore = 0;
+            Random rnd = new Random();      //for allocating bombs at random places in the specified interval
+            int bomb1 = rnd.Next(1, 64);
+            int bomb2 = rnd.Next(1, 64);
+            int bomb3 = rnd.Next(1, 64);
+            int bomb4 = rnd.Next(1, 64);
+            int bomb5 = rnd.Next(1, 64);
+            int bomb6 = rnd.Next(1, 64);
+            int bomb7 = rnd.Next(1, 64);
+            int bomb8 = rnd.Next(1, 64);
+            int bomb9 = rnd.Next(1, 64);
+            int bomb10 = rnd.Next(1, 64);
+            int bomb11 = rnd.Next(1, 64);
+            int bomb12 = rnd.Next(1, 64);
+
+
+            for (int i = 1; i <= 64; i++)
+            {
+                Button btnTemp = new Button();
+                
+                //btnTemp.Text = mineScore.ToString();
+                mineScore = 0;
+
+                btnTemp.Size = new System.Drawing.Size(30, 30);     //makes a matrix that fits in flow layout panel
+
+                if (bomb1 == i - 9 || bomb1 == i - 8 || bomb1 == i - 7 || bomb1 == i - 1 || bomb1 == i + 1 || bomb1 == i + 7 || bomb1 == i + 8 || bomb1 == i + 9)
+                    mineScore++;
+                if (bomb2 == i - 9 || bomb2 == i - 8 || bomb2 == i - 7 || bomb2 == i - 1 || bomb2 == i + 1 || bomb2 == i + 7 || bomb2 == i + 8 || bomb2 == i + 9)
+                    mineScore++;
+                if (bomb3 == i - 9 || bomb3 == i - 8 || bomb3 == i - 7 || bomb3 == i - 1 || bomb3 == i + 1 || bomb3 == i + 7 || bomb3 == i + 8 || bomb3 == i + 9)
+                    mineScore++;
+                if (bomb4 == i - 9 || bomb4 == i - 8 || bomb4 == i - 7 || bomb4 == i - 1 || bomb4 == i + 1 || bomb4 == i + 7 || bomb4 == i + 8 || bomb4 == i + 9)
+                    mineScore++;
+                if (bomb5 == i - 9 || bomb5 == i - 8 || bomb5 == i - 7 || bomb5 == i - 1 || bomb5 == i + 1 || bomb5 == i + 7 || bomb5 == i + 8 || bomb5 == i + 9)
+                    mineScore++;
+                if (bomb6 == i - 9 || bomb6 == i - 8 || bomb6 == i - 7 || bomb6 == i - 1 || bomb6 == i + 1 || bomb6 == i + 7 || bomb6 == i + 8 || bomb6 == i + 9)
+                    mineScore++;
+                if (bomb7 == i - 9 || bomb7 == i - 8 || bomb7 == i - 7 || bomb7 == i - 1 || bomb7 == i + 1 || bomb7 == i + 7 || bomb7 == i + 8 || bomb7 == i + 9)
+                    mineScore++;
+                if (bomb8 == i - 9 || bomb8 == i - 8 || bomb8 == i - 7 || bomb8 == i - 1 || bomb8 == i + 1 || bomb8 == i + 7 || bomb8 == i + 8 || bomb8 == i + 9)
+                    mineScore++;
+                if (bomb9 == i - 9 || bomb9 == i - 8 || bomb9 == i - 7 || bomb9 == i - 1 || bomb9 == i + 1 || bomb9 == i + 7 || bomb9 == i + 8 || bomb9 == i + 9)
+                    mineScore++;
+                if (bomb10 == i - 9 || bomb10 == i - 8 || bomb10 == i - 7 || bomb10 == i - 1 || bomb10 == i + 1 || bomb10 == i + 7 || bomb10 == i + 8 || bomb10 == i + 9)
+                    mineScore++;
+                if (bomb11 == i - 9 || bomb11 == i - 8 || bomb11 == i - 7 || bomb11 == i - 1 || bomb11 == i + 1 || bomb11 == i + 7 || bomb11 == i + 8 || bomb11 == i + 9)
+                    mineScore++;
+                if (bomb12 == i - 9 || bomb12 == i - 8 || bomb12 == i - 7 || bomb12 == i - 1 || bomb12 == i + 1 || bomb12 == i + 7 || bomb12 == i + 8 || bomb12 == i + 9)
+                    mineScore++;
+
+
+
+                
+                btnTemp.ForeColor = Color.AntiqueWhite;
+                if (bomb1 == i || bomb2 == i || bomb3 == i || bomb4 == i || bomb5 == i || bomb6 == i || bomb7 == i || bomb8 == i || bomb9 == i || bomb10 == i || bomb11 == i || bomb12 == i)
+                {
+                    btnTemp.Tag = true;
+                    
+                }
+                else
+                {
+                    btnTemp.Tag = false;
+                    btnTemp.Text = mineScore.ToString();
+                    
+                }
+
+
+                
+                btnTemp.Click += BtnTemp_Click;     //indicates a click has occured
+                flowLayoutPanel1.Controls.Add(btnTemp);
+                
+
+                Console.WriteLine(mineScore);
+            }
+        }
+
+        private void BtnTemp_Click(object sender, EventArgs e)      //for matrix buttons
+        {
+            Button btnTemp = (Button)sender;        //assign a button to object
+            bool tag = (bool)btnTemp.Tag;           //assigns a tag to store the value and check above condition
+            
+            if (tag)
+            {
+                
+                btnTemp.BackColor = Color.Red;
+                btnTemp.ForeColor = Color.AntiqueWhite;//red for bomb
+                int score = int.Parse(lblBomb.Text);        //converting string to int to display bomb when it bursts
+                score++;    
+                lblBomb.Text = score.ToString();        //Bomb gets one
+
+                if (score == 1)        //whenever bomb is pressed it asksyou for restart
+                {
+                    DialogResult result = MessageBox.Show("You are lost\nRestart ?", "Game Result", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+                    if (result == DialogResult.Yes)
+                        Application.Restart();
+                    else
+                        Close();
+                }
+            }
+            else
+            {
+                //btnTemp.Text=mineScore.ToString();
+                btnTemp.BackColor = Color.Green;        //green for a non-bomb box
+                btnTemp.ForeColor = Color.AntiqueWhite;
+                int score = int.Parse(lblScore.Text);       
+                score++;        //the score gets incremented until bomb is pressed
+                lblScore.Text = score.ToString();
+            }
+        }
+        private void BtnTemp2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
